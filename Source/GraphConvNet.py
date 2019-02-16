26443ce1b19df70dd972f9ce3fd908a81d0328b0
Nhat Pham <hoangnhat123456@live.com>
2019-02-15 23:09:17 -0800

    Implement Graph Convolutional Network.

---
+++ Source/GraphConvNet.py
@@ -0,0 +1,93 @@
+import networkx as nx
+import numpy as np
+import torch
+from torch import nn
+from torch.optim import Adam
+
+
+
+
+class GraphConvNet(nn.Module):
+    def __init__(self, in_features, n_class, normed_laplacian):
+        super(GraphConvNet, self).__init__()
+        self._normed_laplacian = torch.from_numpy(normed_laplacian).float()
+        self.add_module(
+            "main",
+            nn.Sequential(
+                GraphConvolutionLayer(
+                    in_features = in_features,
+                    out_features = 256,
+                    normed_laplacian = self._normed_laplacian,
+                    loss = nn.ReLU
+                ),
+                GraphConvolutionLayer(
+                    in_features = 256,
+                    out_features = n_class,
+                    normed_laplacian = self._normed_laplacian
+                ),
+
+            )
+        )
+        self._loss = nn.CrossEntropyLoss()
+
+    def forward(self, input):
+        return self._modules["main"](input)
+
+    def fit(
+            self, input, label,
+            n_train, n_epoch = 5000
+    ):
+        self.train()
+
+        input = torch.from_numpy(input).float()
+        label = torch.from_numpy(label).long()
+        optimizer = Adam(self.parameters(), lr = 0.001)
+
+
+        for e in range(n_epoch):
+            output = self.forward(input)[:n_train]
+            loss = self._loss(output, label)
+
+            print("Epoch " + str(e) + " with loss " + str(loss.item()))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        return
+
+    def predict(self, input):
+        self.eval()
+        op = self.forward(torch.from_numpy(input).float()).detach().numpy()
+        return np.argmax(op, axis = -1)
+
+
+
+
+
+
+
+class GraphConvolutionLayer(nn.Sequential):
+    def __init__(self, in_features, out_features, normed_laplacian, loss = None):
+        super(GraphConvolutionLayer, self).__init__()
+        self.add_module(
+            "main",
+            nn.Linear(in_features = in_features, out_features = out_features),
+        )
+
+        if loss is not None:
+            self.add_module(
+                "loss",
+                loss()
+            )
+            # self.add_module("norm", nn.BatchNorm1d(num_features = out_features))
+
+
+
+
+
+        self._normed_laplacian = normed_laplacian
+
+    def forward(self, input):
+        return super(GraphConvolutionLayer, self).forward(torch.mm(self._normed_laplacian, input))
+
---
+++ main.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+from Source import GraphConvNet
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.decomposition import PCA
+from sklearn.metrics import accuracy_score
+
+
+
+df_edges_list = pd.read_csv("./Data/edges_list.csv")
+# labels = pd.read_csv("./Data/department_labels.csv")[["department_id"]].values - 1
+df_train = pd.read_csv("./Data/train.csv").fillna(0)
+df_test = pd.read_csv("./Data/test.csv").fillna(0)
+df_features = pd.concat((df_train, df_test))
+
+features = df_features.drop(["name", "department", "true_department"], axis = 1).values
+departments = (df_features[["true_department"]].values).reshape(-1)
+n_test = df_test.shape[0]
+n_train = df_train.shape[0]
+
+features[:, :4] = features[:, :4] - 1
+
+
+transformer = ColumnTransformer(
+    [
+        ("oh_0", OneHotEncoder(categories = [range(45)]), [0]),
+        ("oh_1", OneHotEncoder(categories = [range(32)]), [1]),
+        ("oh_2", OneHotEncoder(categories = [range(136)]), [2]),
+        ("oh_3", OneHotEncoder(categories = [range(26)]), [3]),
+
+    ]
+)
+features = transformer.fit_transform(features).toarray()
+print(features.shape)
+
+scaler = StandardScaler()
+features = scaler.fit_transform(features)
+
+
+pca = PCA()
+features_pca = pca.fit_transform(features)
+
+departments_train = departments[:n_train]
+print(np.min(departments_train))
+departments_test = departments[n_train:]
+print(departments_train.shape)
+print(departments_test.shape)
+
+
+
+G = nx.convert_matrix.from_pandas_edgelist(
+    df = df_edges_list,
+    source = "from",
+    target = "to"
+)
+adj_tilde = nx.adjacency_matrix(G).toarray() + np.eye(len(G.nodes), len(G.nodes))
+deg_tilde = np.sum(adj_tilde, axis=0)
+deg_tilde_inv_sqrt = np.diag(1.0 / np.sqrt(deg_tilde))
+normed_laplacian = deg_tilde_inv_sqrt * adj_tilde * deg_tilde_inv_sqrt
+
+model = GraphConvNet(in_features = features.shape[1], normed_laplacian = normed_laplacian, n_class = 42)
+model.fit(features, departments_train, n_train, n_epoch = 500)
+
+prediction = model.predict(features)
+print(prediction.shape)
+print(accuracy_score(departments_test, prediction[n_train:]))
